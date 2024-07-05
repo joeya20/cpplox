64a88a5b2cc6af867ea4b09de376cf9c66b67fca
Joey Ah-kiow <joeyah20@gmail.com>
2024-07-05 15:33:04 -0400

    finished basic implementation of exprs

--- scripts/gen_expr.py
+++ scripts/gen_expr.py
@@ -1,38 +1,39 @@
 from argparse import ArgumentParser
-from typing import Dict, Sequence
+from typing import Dict
 from pathlib import Path
 
 
 EXPR_TMPL = """
+#pragma once
 
 #include "Token.hh"
 #include <memory>
 
 
-struct {base_name} {
+struct {base_name} {{
   virtual void f() = 0;
-};
+}};
 
-{sub_types}
+{sub_exprs}
 
 """
 
-TYPE_TMPL = """
-
-struct {class_name} : {base_name} {{
+SUB_EXPR_TMPL = """
+struct {class_name}{base_name} : {base_name} {{
   void f() override {{}}
 
   {fields}
 
-  {class_name}() {{
+  {class_name}{base_name}({args}) {{
     {inits}
   }}
-}}
+}};
 
 """
-
+ARGS_TMPL = "std::unique_ptr<{field_type}>&& {field_name}"
 TYPE_FIELD_TMPL = "std::unique_ptr<{field_type}> {field_name};"
-INIT_TMPL = "std::unique_ptr<{field_type}> {field_name} = {field_name};"
+INIT_TMPL = "this->{field_name} = std::move({field_name});"
+
 
 def define_type(base_name, class_name, field_list):
   fields = "\n\t".join([
@@ -42,43 +43,53 @@ def define_type(base_name, class_name, field_list):
     for field in field_list
   ])
   inits = "\n\t\t".join([
-    TYPE_FIELD_TMPL.format(
+    INIT_TMPL.format(
       field_type=field.split()[0], field_name=field.split()[1]
     )
     for field in field_list
   ])
-  return TYPE_TMPL.format(
+  args = ", ".join([
+    ARGS_TMPL.format(
+      field_type=field.split()[0], field_name=field.split()[1]
+    )
+    for field in field_list
+  ])
+  return SUB_EXPR_TMPL.format(
     class_name=class_name,
     base_name=base_name,
     fields=fields,
+    args=args,
     inits=inits
   )
 
 
-def define_ast(output_dir, base_name, types: Sequence[Dict]):
+def define_ast(output_dir, base_name, sub_exprs: Dict):
     filepath = Path(output_dir, base_name + ".hh")
-    for k, v in types.items():
-      sub_classes = "".join([define_type(sub_type, )])
+    sub_exprs_str = ""
+    for sub_expr_name, fields in sub_exprs.items():
+      sub_exprs_str += "".join([define_type(base_name, sub_expr_name, fields)])
     impl = EXPR_TMPL.format(
       base_name=base_name,
+      sub_exprs=sub_exprs_str
     )
 
     with open(filepath, 'w') as output_file:
       output_file.write(impl)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
   arg_parser = ArgumentParser(
     prog='Generate Expr.hh',
     description='Script to generate Expr.hh'
   )
-  arg_parser.add_argument('-o', '--output', action='store', type=str)
+  arg_parser.add_argument('output_dir', action='store', type=str)
   args = arg_parser.parse_args()
-  output_dir = args.output
+  output_dir = args.output_dir
 
-  define_ast(output_dir, "Expr", [
-      "Binary   : Expr left, Token operator, Expr right",
-      "Grouping : Expr expression",
-      "Literal  : Object value",
-      "Unary    : Token operator, Expr right"
-  ])
+  define_ast(output_dir, "Expr", {
+      "Binary":   ["Expr left", "Token op", "Expr right"],
+      "Grouping": ["Expr expression"],
+      "Unary":    ["Token op", "Expr right"],
+      "FloatLit":  ["float value"],
+      "StringLit":  ["std::string value"]
+  })
--- src/Expr.hh
+++ src/Expr.hh
@@ -1,3 +1,6 @@
+
+#pragma once
+
 #include "Token.hh"
 #include <memory>
 
@@ -11,12 +14,25 @@ struct BinaryExpr : Expr {
   void f() override {}
 
   std::unique_ptr<Expr> left;
-  Token op;
-  std::unique_ptr<Expr> right;
+	std::unique_ptr<Token> op;
+	std::unique_ptr<Expr> right;
 
-  BinaryExpr(std::unique_ptr<Expr> left, Token op, std::unique_ptr<Expr> right) :
-    left(std::move(right)), op(op), right(std::move(right)) {};
+  BinaryExpr(std::unique_ptr<Expr>&& left, std::unique_ptr<Token>&& op, std::unique_ptr<Expr>&& right) {
+    this->left = std::move(left);
+		this->op = std::move(op);
+		this->right = std::move(right);
+  }
+};
 
+
+struct GroupingExpr : Expr {
+  void f() override {}
+
+  std::unique_ptr<Expr> expression;
+
+  GroupingExpr(std::unique_ptr<Expr>&& expression) {
+    this->expression = std::move(expression);
+  }
 };
 
 
@@ -24,7 +40,35 @@ struct UnaryExpr : Expr {
   void f() override {}
 
   std::unique_ptr<Token> op;
-  std::unique_ptr<Expr> operand;
+	std::unique_ptr<Expr> right;
 
-  UnaryExpr(Expr operand, Token op);
+  UnaryExpr(std::unique_ptr<Token>&& op, std::unique_ptr<Expr>&& right) {
+    this->op = std::move(op);
+		this->right = std::move(right);
+  }
 };
+
+
+struct FloatLitExpr : Expr {
+  void f() override {}
+
+  std::unique_ptr<float> value;
+
+  FloatLitExpr(std::unique_ptr<float>&& value) {
+    this->value = std::move(value);
+  }
+};
+
+
+struct StringLitExpr : Expr {
+  void f() override {}
+
+  std::unique_ptr<std::string> value;
+
+  StringLitExpr(std::unique_ptr<std::string>&& value) {
+    this->value = std::move(value);
+  }
+};
+
+
+
